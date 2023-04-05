ec9e822d565ace1165eb999f51dac5a891582611
HeitorPontieri <heitorpontieri1@gmail.com>
2023-04-05 10:24:39 -0300

    working

--- src/App.js
+++ src/App.js
@@ -1,68 +1,63 @@
-import "./styles.css";
-import useLagRadar from "./useLagRadar";
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
-import Area from "./components/Area";
-import Band from "./components/Band";
-import Bar from "./components/Bar";
-import BarStacked from "./components/BarStacked";
-import Bubble from "./components/Bubble";
-import CustomStyles from "./components/CustomStyles";
-import DarkMode from "./components/DarkMode";
-import DynamicContainer from "./components/DynamicContainer";
-import InteractionMode from "./components/InteractionMode";
-import Line from "./components/Line";
-import MultipleAxes from "./components/MultipleAxes";
-import Steam from "./components/Steam";
-import BarHorizontal from "./components/BarHorizontal";
-import BarHorizontalStacked from "./components/BarHorizontalStacked";
-import SparkChart from "./components/SparkChart";
-import SyncedCursors from "./components/SyncedCursors";
-import StressTest from "./components/StressTest";
-
-
-const components = [
-    ["Line", Line],
-    ["Bar", Bar],
-    ["Bar (Stacked)", BarStacked],
-    ["Bar (Horizontal)", BarHorizontal],
-    ["Bar (Horizontal + Stacked)", BarHorizontalStacked],
-    ["Band", Band],
-    ["Area", Area],
-    ["Bubble", Bubble],
-    ["Steam", Steam],
-    ["Spark Chart", SparkChart],
-    ["Multiple Axes", MultipleAxes],
-    ["Interaction Modes", InteractionMode],
-    ["Dark Mode", DarkMode],
-    ["Dynamic / Overflow Container", DynamicContainer],
-    ["Custom Styles", CustomStyles],
-    ["Synced Cursors", SyncedCursors],
-    ["Stress Test", StressTest],
+const data = [
+  {
+    name: 'Page A',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Page B',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'Page C',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Page D',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'Page E',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'Page F',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'Page G',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
 ];
 
 export default function App() {
-
-    return ( <
-        div > {
-            components.map(([label, Comp]) => {
-                return ( <
-                    div key = { label + "" } >
-                    <
-                    h1 > { label } < /h1> <
-                    div >
-                    <
-                    Comp / >
-                    <
-                    /div> <
-                    /div>
-                );
-            })
-        } <
-        div style = {
-            { height: "50rem" } }
-        /> <
-        /div>
-    );
+  return (
+    <div>
+        <LineChart width={1730} height={1250} data={data}
+          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend verticalAlign="bottom" height={36}/>
+          <Line type="monotone" dataKey="pv" stroke="#143601"/>
+        </LineChart>
+    </div>
+  );
 }
